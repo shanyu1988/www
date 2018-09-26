148805f7b410df25c3afb5785d38a9cec01ff18a
home_pc_01 <359987011@qq.com>
2018-09-26 14:09:51 +0800

    日常跟新

--- php/test/test06/SQL.php
+++ php/test/test06/SQL.php
@@ -1,3 +1,9 @@
 <?php
 $conection=mysqli_connect('47.96.77.249','root','Shanyu_1988','my');
-var_dump($conection);
+
+if(!$conection){
+    exit('<h1>连接数据库失败</h1>');
+}
+
+$query=mysqli_query($conection,'select * from users');
+
