4545d7504d8d67be0024067f65567ea282d2b1c7
home_pc_01 <359987011@qq.com>
2018-09-26 18:00:16 +0800

    日常更新

--- php/test/test06/SQL.php
+++ php/test/test06/SQL.php
@@ -1,9 +1,18 @@
 <?php
+
 $conection=mysqli_connect('47.96.77.249','root','Shanyu_1988','my');
 
 if(!$conection){
     exit('<h1>连接数据库失败</h1>');
 }
 
-$query=mysqli_query($conection,'select * from users');
+$query=mysqli_query($conection,'SELECT * FROM `users`;');
 
+if(!$query){
+    exit('<h1>查询数据失败</h1>');
+}
+$arr=array();
+while($row = mysqli_fetch_assoc($query)){
+        $arr[]=$row;
+}
+var_dump($arr);
