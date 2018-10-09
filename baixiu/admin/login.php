9c3135fe017e76a4045524e96e1ca8be61dcc4ea
home_pc_01 <359987011@qq.com>
2018-10-09 17:13:07 +0800

    新增退出功能

--- baixiu/admin/login.php
+++ baixiu/admin/login.php
@@ -1,40 +1,45 @@
 <?php
 require_once '../config.php';
-function login(){
-    if(empty($_POST['username'])){
-        $GLOBALS['message']='请填写邮箱';
+session_start();
+function login()
+{
+    if (empty($_POST['username'])) {
+        $GLOBALS['message'] = '请填写邮箱';
         return;
     }
-    if(empty($_POST['password'])){
-        $GLOBALS['message']='请填写密码';
+    if (empty($_POST['password'])) {
+        $GLOBALS['message'] = '请填写密码';
         return;
     }
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    $query=mysqli_query($conn,"SELECT * FROM `users` where `email`='{$username}' limit 1;");
-    if(!$query){
+    $conn = mysqli_connect(XIU_DB_HOST, XIU_DB_USER, XIU_DB_PASS, XIU_DB_NAME);
+    $query = mysqli_query($conn, "SELECT * FROM `users` where `email`='{$username}' limit 1;");
+    if (!$query) {
         $GLOBALS['message'] = '登录失败，请重试！';
         return;
     }
-    $user=mysqli_fetch_assoc($query);
-    if(!$user){
+    $user = mysqli_fetch_assoc($query);
+    if (!$user) {
         $GLOBALS['message'] = '邮箱与密码不匹配！';
         return;
     }
-    if($user['password']!==$password){
+    if ($user['password'] !== $password) {
         $GLOBALS['message'] = '邮箱与密码不匹配！';
         return;
     }
-    header('location: index.php');
-
-
+    $_SESSION['login_user'] = $user;
+    header('Location: index.php');
 }
 
-    if($_SERVER['REQUEST_METHOD']==='POST'){
-        login();
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    login();
+}
+//action=logout
+if($_SERVER["REQUEST_METHOD"]==='GET'&&isset($_GET['action'])&&$_GET['action']==='logout'){
+    unset($_SESSION['login_user']);
+}
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -49,10 +54,10 @@ function login(){
     <form class="login-wrap" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" novalidate autocomplete="off">
         <img class="avatar" src="/static/assets/img/default.png">
         <!-- 有错误信息时展示 -->
-        <?php if(isset($message)):?>
-        <div class="alert alert-danger">
-            <strong>错误！</strong> <?php echo $message; ?>
-        </div>
+        <?php if (isset($message)): ?>
+            <div class="alert alert-danger">
+                <strong>错误！</strong> <?php echo $message; ?>
+            </div>
         <?php endif ?>
         <div class="form-group">
             <label for="email" class="sr-only">邮箱</label>
@@ -65,5 +70,26 @@ function login(){
         <button class="btn btn-primary btn-block">登 录</button>
     </form>
 </div>
+<script src="/static/assets/vendors/jquery/jquery.js"></script>
+<script>
+    $(function ($) {
+        $('#email').on('blur', function () {
+            var value = $(this).val();
+            var emailFormat = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
+            if (!value || !emailFormat.test(value)) return;
+
+            $.get('/admin/api/portrait.php',{email:value},function (res) {
+                if(!res) return;
+                console.log(res);
+                $('.avatar').fadeOut(function () {
+                    $(this).attr('src',res).on('load',function () {
+                        $(this).fadeIn();
+                    })
+                })
+            })
+        });
+
+    })
+</script>
 </body>
 </html>
